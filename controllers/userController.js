439a807d845c74e877a825172f5a86d52d1d1c72
Mays88 <antmays.ceo@gmail.com>
2024-05-17 13:55:09 -0400

    Completed setup of User routes to MongoDB

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,26 +1,44 @@
 // import { users } from "../data/users.js";
 import db from "../db/conn.js";
-export const getUsers = (req, res) => {
-    res.status(500).json({
-        status: "error",
-        message: "Route handler not created yet",
-    });
+import { ObjectId } from "mongodb";
+
+const users = await db.collection("users");
+export const getUsers = async (req, res) => {
+    const result = await users.find().toArray();
+
+    if (!result) res.send("Not Found").status(404);
+    else
+        res.status(200).json({
+            status: "Success",
+            results: result.length,
+            data: { result },
+        });
 };
 
-export const getUser = (req, res) => {
-    res.status(500).json({
-        status: "error",
-        message: "Route handler not created yet",
+export const getUser = async (req, res) => {
+    const query = { _id: new ObjectId(req.params.id) };
+    const user = await users.findOne(query);
+
+    if (!user) {
+        return res.status(404).json({
+            status: "Fail",
+            message: "Invalid ID",
+        });
+    }
+
+    res.status(200).json({
+        status: "Success",
+        data: {
+            user,
+        },
     });
 };
 
 export const createUser = async (req, res) => {
-    const collection = await db.collection("users");
     const user = req.body;
 
-    const result = await collection.insertOne(user);
+    const result = await users.insertOne(user);
 
-    // users.push(newUser);
     res.status(201).json({
         status: "Success",
         data: {
@@ -29,16 +47,34 @@ export const createUser = async (req, res) => {
     });
 };
 
-export const updateUser = (req, res) => {
-    res.status(500).json({
-        status: "error",
-        message: "Route handler not created yet",
+export const updateUser = async (req, res) => {
+    const query = { _id: new ObjectId(req.params.id) };
+
+    let user = await users.updateMany(query, {
+        $set: req.body,
+    });
+
+    res.status(200).json({
+        status: "Success",
+        data: {
+            user,
+        },
     });
 };
 
-export const deleteUser = (req, res) => {
-    res.status(500).json({
-        status: "error",
-        message: "Route handler not created yet",
+export const deleteUser = async (req, res) => {
+    const query = { _id: new ObjectId(req.params.id) };
+    const user = await users.deleteOne(query);
+
+    if (!user) {
+        return res.status(404).json({
+            status: "fail",
+            message: "Invalid ID",
+        });
+    }
+
+    res.status(204).json({
+        status: "Success",
+        data: null,
     });
 };
